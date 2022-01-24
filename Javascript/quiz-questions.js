fece4f7753b8d4c2665d4d5de806c7b870615a0f
WaleeA <58791789+WaleeA@users.noreply.github.com>
2022-01-24 15:25:03 +0000

    Add files via upload

---
+++ Javascript/quiz-questions.js
@@ -0,0 +1,56 @@
+// Title: Simple Javascript Quiz
+// Author: Code-boxx.com
+//Date: 2020
+//Availablity: https://code-boxx.com/simple-javascript-quiz/
+var questions = [
+  {
+    q : "When was Cloud computing invented",
+    o : [
+      "2010s",
+      "1960s",
+      "2000s",
+      "2020s"
+    ],
+    a : 1 // arrays start with 0, so it is 70 meters
+  },
+  {
+    q : "What is the range limit from server to user with cloud computing",
+    o : [
+      "1 kilometre",
+      "100 metres",
+      "1000 kilometres",
+      "No range limit"
+    ],
+    a : 3
+  },
+  {
+    q : "With increased use of Virtual Computers, when did Cloud Computing become popular",
+    o : [
+      "1970s",
+      "2000s",
+      "1990s",
+      "1962"
+    ],
+    a : 2
+  },
+  {
+    q : "Which two companies currently have readily available cloud computing software?",
+    o : [
+      "Amazon and Microsoft",
+      "Apple and Sony",
+      "Tesco and Morrisons",
+      "LG and Sonos"
+    ],
+    a : 0
+  },
+  {
+    q : "Which of these are a disadvantage of Cloud Computing?",
+    o : [
+      "Data not readily available offline",
+      "Can be costly to setup and run",
+      "Data can be hacked",
+      "All of the above"
+    ],
+    a : 3
+  }
+];
